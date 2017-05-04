1223d3634df09e0cd2f9aef5af862d7b8d3b198a
Makemore2014 <Makemore2014@outlook.com>
2017-05-04 13:37:59 +0800

    K-Means Cluster Web  Application

--- ui.R
+++ ui.R
@@ -1,19 +1,27 @@
-library(shiny)
-#Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
-  #Application title
-  headerPanel("Miles Per Gallon"),
-  sidebarPanel(
-    selectInput("variable", "Variable:",
-                list("Cylinders"="cyl",
-                     "Transmission"="am",
-                     "Gears"="gear")),
-    checkboxInput("outliers", "show outliers", FALSE)
-  ),
-  mainPanel(
-    h3(textOutput("caption")),
-    
-    plotOutput("mpgplot")
-  )
 
-))
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+library(shiny)
+library(shinydashboard)
+library()
+dashboardPage(
+  dashboardHeader(title = "Iris K-Means cluster"),
+  dashboardSidebar(
+    selectInput('xcol', 'X  variable', names(iris)[1:4]),
+    selectInput('ycol', 'Y variable', names(iris)[1:4],
+                selected = names(iris)[[2]]),
+    numericInput('clusters', label = 'Cluster Count', value = 3,min=1,max=9)
+  ),
+  dashboardBody(
+   # column(4,plotOutput("plot1"))
+    box(title = "K-Means cluster plot",status = 'success',
+        plotOutput("plot1"), width = 6),
+    #column(4,DT::dataTableOutput("DT"))
+    box(title = "use DT package plot",width = 4,
+        DT::dataTableOutput("DT"), status = 'info',collapsible = T, solidHeader = T),
+    column(4,tableOutput("head"))
+  )
+)
